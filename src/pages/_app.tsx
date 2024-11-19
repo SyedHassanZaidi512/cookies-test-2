fa5d860474add40ae651a01de7a017ef43752dfe
hassan zaidi <syed.hassan.raza677@gmail.com>
2024-11-19 18:19:46 +0500

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import Cookies from 'js-cookie';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+
+export default function App() {
+  const handleClick = () => {
+    console.log(">>>handleClick");
+    const expiryDate = new Date();
+    expiryDate.setHours(expiryDate.getHours() + 1);
+
+    Cookies.set('idToken',"abc", {
+      sameSite:  'none',
+      secure: true,
+      expires: expiryDate
+    });
+
+    window.location.href = "http://localhost:3003"
+
+  }
+  return <>
+    <div onClick={handleClick} style={{width:'300px', height:'200px'}}>
+      click here to navigate
+    </div>
+  </>;
 }
